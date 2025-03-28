7f282167b3d41ecb94bb82ca3aecddad41b13290
Git4course <javacoursework942@gmail.com>
2025-03-28 17:36:10 +0530

    Initial project setup with basic classes.

---
+++ src/main/java/com/boostphysioclinic/BoostPhysioClinicalApplication.java
@@ -0,0 +1,7 @@
+package com.boostphysioclinic;
+
+
+public class BoostPhysioClinicalApplication {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/test/java/BoostPhysioClinicalTest.java
@@ -0,0 +1,10 @@
+
+import com.boostphysioclinic.model.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BoostPhysioClinicalTest {
+
+}
